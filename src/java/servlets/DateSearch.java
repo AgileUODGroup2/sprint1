d69bfa9fa9848b1d2f245e81d854f77a249756c5
viivipursiainen <vkpursiainen@gmail.com>
2017-02-18 17:15:55 +0000

    Results can be searched from student_quiz table by giving two dates Returns all the results that have been completed between given dates

--- src/java/servlets/DateSearch.java
+++ src/java/servlets/DateSearch.java
@@ -6,12 +6,21 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.ResultModel;
+import stores.Result;
 
 /**
  *
@@ -29,6 +38,35 @@ public class DateSearch extends HttpServlet {
         String month2 = request.getParameter("month2");
         String year1 = request.getParameter("year1");
         String year2 = request.getParameter("year2");
+        
+        String stringDate1 = year1 + "-" + month1 + "-" + day1;
+        String stringDate2 = year2 + "-" + month2 + "-" + day2;
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date javaDate1;
+        java.util.Date javaDate2;
+        Date date1, date2;
+        date1 = date2 = null;
+        
+        try {
+            javaDate1 = df.parse(stringDate1);
+            javaDate2 = df.parse(stringDate2);
+            date1 = new Date(javaDate1.getTime());
+            date2 = new Date(javaDate2.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        
+        ResultModel rm = new ResultModel();
+        java.util.LinkedList<Result> resultList = null;
+        try {
+            resultList = rm.getResultsForDates(date1, date2);
+        } catch (SQLException ex) {
+            Logger.getLogger(DateSearch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("/quizResults.jsp");
+        request.setAttribute("ResultList", resultList);
+        rd.forward(request, response);
         
     }
 
