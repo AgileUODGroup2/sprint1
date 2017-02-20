148578dda22f1ef07acb5e0c1498805035dc3736
ashawittchen <asha.wittchen@hotmail.com>
2017-02-20 12:17:03 +0000

    Merge origin/master
    Conflicts:
    	web/staffPortal.jsp

---
+++ src/java/servlets/wholeStudentList.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import models.wholeStudentListModel;
+/**
+ *
+ * @author ashawittchen
+ */
+
+@WebServlet(name ="wholeStudentList", urlPatterns = {"/wholeStudentList"})
+
+public class wholeStudentList extends HttpServlet {
+    public void init(ServletConfig config) throws ServletException {
+        // TODO Auto-generated method stub
+        
+    }
+    
+ @Override
+protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+
+   wholeStudentListModel wholeStudentLt = new wholeStudentListModel(); 
+   wholeStudentLt.getWholeStudentList();
+}
+   
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        RequestDispatcher rd=request.getRequestDispatcher("wholeStudentList.jsp");
+	    rd.forward(request,response);
+    }
+    
+}
