5ec07b6174cbb01e3518a93eb440fbd343cd7659
erincoey <ecoey332@gmail.com>
2017-03-09 19:26:18 +0000

    Taking a quiz - all questions displayed at one time

--- src/java/servlets/takeQuiz.java
+++ src/java/servlets/takeQuiz.java
@@ -34,29 +34,62 @@ public class takeQuiz extends HttpServlet{
         String strQuizID = uri.substring(i+1);
         int quizID = Integer.parseInt(strQuizID);
         System.out.println("Quiz ID: "+quizID);
-        display(quizID, request, response);
+            display(quizID, request, response);
 
     }
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
-      
+        String counter = request.getParameter("counter");
+        int i = Integer.parseInt(counter);
         
+        String[] parameters = new String[i];
+
+        for(int x=0; x<i; x++)
+        {
+            String parameter = "answer"+x;
+            parameters[x] = parameter;
+            System.out.println(parameter);
+        }
+        for(int x=0; x<i; x++)
+        {
+            System.out.println(parameters[x]);
+            System.out.println(request.getParameter(parameters[x]));
+        }
+        
+        
+        
+        response.sendRedirect("/AC31007Quiz/studentPortal.jsp");
+       
+      
     }
    private void display(int quizID, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         ResultModel rm = new ResultModel();
         QuizModel qm = new QuizModel();
-
+//        String test;
         
         HttpSession session = request.getSession(true);
         LoggedIn lg =(LoggedIn)session.getAttribute("LoggedIn");
         
+//        String button = request.getParameter("button");
+//        if (button=="One Question at a time")
+//        {
+//            test = "oneQuestion";
+//            System.out.println("test: " + test);
+//            request.setAttribute("test", test);
+//        }
+//        else if(button == "All questions")
+//        {
+//            test = "allQuestions";
+//            System.out.println("test: " + test);
+//            request.setAttribute("test", test);
+//        }
         Quiz quiz = qm.getQuizDetails(quizID);
       
             
-            RequestDispatcher rd = request.getRequestDispatcher("/takeQuiz.jsp"); // SET CORRECT REDIRECT LOCATION
+            RequestDispatcher rd = request.getRequestDispatcher("/takeQuiz.jsp"); 
             
             request.setAttribute("Quiz", quiz);
             rd.forward(request, response);
