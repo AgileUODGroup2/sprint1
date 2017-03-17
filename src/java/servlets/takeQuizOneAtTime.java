e491679845001e2a62f23a9fe2e19bc763878ffb
glenmorrison <glenmorrison@labpc012.computing.dundee.ac.uk>
2017-03-17 16:55:24 +0000

    Jump to question working . 2

--- src/java/servlets/takeQuizOneAtTime.java
+++ src/java/servlets/takeQuizOneAtTime.java
@@ -6,7 +6,9 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.Arrays;
+import java.util.Calendar;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +16,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.AttemptModel;
+import models.QuestionModel;
 import models.QuizModel;
 import models.ResultModel;
 import stores.LoggedIn;
@@ -28,120 +32,134 @@ public class takeQuizOneAtTime extends HttpServlet {
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         HttpSession session = request.getSession();
         String[] studentAnswers = (String[]) session.getAttribute("StudentAnswers");
-        System.out.println("DOGET: student answers :" + Arrays.toString(studentAnswers));
+        int[] qIDs = (int[]) session.getAttribute("QuestionIDs");
+        System.out.println("DO GET: student answers :" + Arrays.toString(studentAnswers));
+        System.out.println("DO GET: question ID's :" + Arrays.toString(qIDs));
         
         String uri = request.getRequestURI();
         int i = uri.lastIndexOf("/");
         String strQuizID = uri.substring(i+1);
         int quizID = Integer.parseInt(strQuizID);
-        System.out.println("Quiz ID: "+quizID);
         display(studentAnswers, request, response);
-
     }
-    
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
         QuizModel qm = new QuizModel();
             
         HttpSession session = request.getSession();
         String[] studentAnswers = (String[]) session.getAttribute("StudentAnswers");
+        int[] qIDs = (int[]) session.getAttribute("QuestionIDs");
+        LoggedIn lg = (LoggedIn) session.getAttribute("LoggedIn");
         
-        //Get ID from page
-        int quizID = Integer.parseInt(request.getParameter("quizID"));
-        
-        //Get total number of questions from page
-        int quiz_totalQuestionAmmount = Integer.parseInt(request.getParameter("numOfQuestions"));
-        
-        //get current question
-        int quiz_currentQuestion = Integer.parseInt(request.getParameter("questionNumber"));
-        
-        //Get all details of current quiz
-        Quiz quiz = qm.getQuizDetails(quizID);
+        int matricNo = lg.getID();
+        int quizID = Integer.parseInt(request.getParameter("quizID")); //Get ID from page
+        int quiz_totalQuestionAmmount = Integer.parseInt(request.getParameter("numOfQuestions"));//Get total number of questions from page
+        int quiz_currentQuestion = Integer.parseInt(request.getParameter("questionNumber"));//get current question
+        Quiz quiz = qm.getQuizDetails(quizID);//Get all details of current quiz
         
         String btn = request.getParameter("next");
         String btnFinish = request.getParameter("Finish Quiz");
+        String btnSave = request.getParameter("Save");
             
         if(btnFinish == null){
-        
             if(btn == null){
-                
-                ///////////////////////////////
                 //BUTTON'S JUMP TO QUESTIONS
-                ///////////////////////////////
-                
                 int jumpQuestion = Integer.parseInt(request.getParameter("jumpQuestion"));
-                
                 for(int p=0;p<quiz_totalQuestionAmmount;p++){
-                    
-                    if(jumpQuestion == p){
-                        
+                    if(jumpQuestion == p)
                         quiz.setCounter(p);
-                        
-                    }
-                    
                 }
-                
-                ////////////////////////////////
-                
+            }
+            else if(btnSave != null){
+                //BUTTON SAVE
+                Save(request,response,studentAnswers,qIDs, matricNo);
             }
             else{
-                
-                ///////////////////////////////
                 //BUTTON NEXT QUESTION
-                ///////////////////////////////
-                
                 studentAnswers[quiz_currentQuestion] = request.getParameter("answer");
-        
                 int incrementQuestionNumber = quiz_currentQuestion;
-                
                 incrementQuestionNumber ++;
-                
                 quiz.setCounter(incrementQuestionNumber);
-                
-                ///////////////////////////////
-                
             }
-            
             session.setAttribute("Quiz", quiz);
-            
             String contextPath = request.getContextPath();
-            
             String url = contextPath+ "/takeQuizOneAtTime/"+quizID;
-            
             System.out.println(url);
-            
             response.sendRedirect(url);
-
         }
         else{
-             
-            ///////////////////////////////
-            //BUTTON FINISH QUIZ
-            ///////////////////////////////
-            
-            System.out.println("POST - User clicked on button Finish Quiz");
-            session.setAttribute("StudentAnswers", studentAnswers);
-
-            ////////////////////////////////
-            
+            studentAnswers[quiz_currentQuestion] = request.getParameter("answer");
+            System.out.println("*** FINAL ANSWERS: student answers :" + Arrays.toString(studentAnswers));
+            System.out.println("*** QUIZ ID's    : question ID's :" + Arrays.toString(qIDs));
+            Submit(request,response,studentAnswers,qIDs, matricNo);
         }
     }
-   private void display(String[] studentAnswers, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       
+    
+    private int calculateResult(String[] studentAnswers, String[] rightAnswers) {
+        int questions = rightAnswers.length;
+        int right = 0;
+        for (int i=0; i<questions;i++) {
+            if(studentAnswers[i] != null && studentAnswers[i].equals(rightAnswers[i])) {
+                right ++;
+            }
+        }
+        int answer = right*100/questions;
+        return answer;
+    }
+    
+    private void Submit(HttpServletRequest request, HttpServletResponse response, String[] studentAnswers, int[] qIDs, int matricNo) throws ServletException, IOException {
+        int quizID = Integer.parseInt(request.getParameter("quizID"));
+        Date date = new Date(Calendar.getInstance().getTime().getTime());
+        
+        QuestionModel questionM = new QuestionModel();
+        String[] rightAnswers = questionM.getRightAnswers(qIDs);
+        
+        int score = calculateResult(studentAnswers, rightAnswers);
+        
+        QuizModel qm = new QuizModel();
+        qm.addNewAttempt(matricNo, quizID, score, date);
+        Quiz quiz = qm.getQuizDetails(quizID);
+        
+        AttemptModel am = new AttemptModel();
+        am.addNewAttempt(matricNo, quizID, date, score);
+        
+        request.setAttribute("StudentAnswers",studentAnswers);
+        request.setAttribute("RightAnswers",rightAnswers);
+        request.setAttribute("QuestionIDs",qIDs);
+        request.setAttribute("Quiz", quiz);
+        request.setAttribute("Score", score);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/showAnswers.jsp");
+        rd.forward(request, response);
+    }
+    
+    private void Save(HttpServletRequest request, HttpServletResponse response, String[] studentAnswers, int[] qIDs, int matricNo) throws ServletException, IOException {
+        QuestionModel questionM = new QuestionModel();
+        
+        for (int j=0; j<studentAnswers.length;j++) {
+            if (studentAnswers[j] != null) {
+                questionM.storeAnswer(studentAnswers[j],qIDs[j],matricNo);
+            }
+        }
+        
+        int quizID = Integer.parseInt(request.getParameter("quizID"));
+        QuizModel qm = new QuizModel();
+        qm.updateStudentQuizStatus(matricNo, quizID, "Incomplete");
+        RequestDispatcher rd = request.getRequestDispatcher("/studentPortal.jsp");
+        rd.forward(request, response);
+    }
+    
+    private void display(String[] studentAnswers, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         Quiz quiz = (Quiz) session.getAttribute("Quiz");
         RequestDispatcher rd = request.getRequestDispatcher("/takeQuizOneAtTime.jsp");
         request.setAttribute("Quiz", quiz);
         request.setAttribute("StudentAnswers", studentAnswers);
-        
         rd.forward(request, response);
-        
     }
-    
+   
 }
 
