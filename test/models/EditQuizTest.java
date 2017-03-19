25e09c74929461c208377a89fc94cc867ea75895
erincoey <ecoey332@gmail.com>
2017-03-19 14:58:46 +0000

    Changes to displaying quizzes

---
+++ test/models/EditQuizTest.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import javax.servlet.http.Part;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import stores.QuestionBank;
+
+/**
+ *
+ * @author erincoey
+ */
+public class EditQuizTest {
+    
+    public EditQuizTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of AlterQuizTitle method, of class EditQuiz.
+     */
+    @Test
+    public void testAlterQuizTitle() throws Exception {
+        
+    }
+
+    /**
+     * Test of AlterQuestionAnswers method, of class EditQuiz.
+     */
+    @Test
+    public void testAlterQuestionAnswers() throws Exception {
+       
+    }
+
+    /**
+     * Test of AlterQuestionAnswersDescription method, of class EditQuiz.
+     */
+    @Test
+    public void testAlterQuestionAnswersDescription() throws Exception {
+        
+    }
+
+    /**
+     * Test of AlterQuestionAnswer method, of class EditQuiz.
+     */
+    @Test
+    public void testAlterQuestionAnswer() throws Exception {
+        
+    }
+
+    /**
+     * Test of DeleteQuestion method, of class EditQuiz.
+     */
+    @Test
+    public void testDeleteQuestion() {
+        
+    }
+
+    /**
+     * Test of EditWholeQuiz method, of class EditQuiz.
+     */
+    @Test
+    public void testEditWholeQuiz() {
+       
+    }
+
+    /**
+     * Test of updateQuestionMedia method, of class EditQuiz.
+     */
+    @Test
+    public void testUpdateQuestionMedia() throws Exception {
+        
+    }
+    
+}
