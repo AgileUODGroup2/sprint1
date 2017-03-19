a4e297def71fe0d61e961bd56da1ad29f3d2aeb9
Viivi Pursiainen <vkpursiainen@gmail.com>
2017-03-19 23:09:03 +0000

    Some debugging done, retrieving saved questions works now.

--- src/java/servlets/takeQuiz.java
+++ src/java/servlets/takeQuiz.java
@@ -46,6 +46,7 @@ public class takeQuiz extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String counter = request.getParameter("counter");
         int i = Integer.parseInt(counter);
+        int quizID = Integer.parseInt(request.getParameter("quizID"));
         
         HttpSession session = request.getSession();
         LoggedIn lg = (LoggedIn) session.getAttribute("LoggedIn");
@@ -53,6 +54,12 @@ public class takeQuiz extends HttpServlet{
         
         String[] studentAnswers = new String[i];
         int[] qIDs = new int[i];
+        
+        QuizModel qm = new QuizModel();
+        if(qm.getStudentStatus(matricNo, quizID).equals("Incomplete")) {
+            AnswerModel ansM = new AnswerModel();
+            ansM.deleteAnswers(qIDs, matricNo);
+        }
 
         for(int x=1; x<=i; x++) {
             studentAnswers[x-1] = request.getParameter("answer"+x);
@@ -60,7 +67,7 @@ public class takeQuiz extends HttpServlet{
         }
         
         String submit = request.getParameter("submit");
-        if (submit.equals("Save")) {
+        if (submit.equals("Save for another time")) {
             save(request,response,studentAnswers,qIDs, matricNo);
         } else {
             submit(request,response,studentAnswers,qIDs, matricNo);
@@ -100,8 +107,6 @@ public class takeQuiz extends HttpServlet{
         am.addNewAttempt(matricNo, quizID, date, score);
         
         if(qm.getStudentStatus(matricNo, quizID).equals("Incomplete")) {
-            AnswerModel ansM = new AnswerModel();
-            ansM.deleteAnswers(qIDs, matricNo);
             qm.updateStudentQuizStatus(matricNo, quizID, "Completed");
         }
         
